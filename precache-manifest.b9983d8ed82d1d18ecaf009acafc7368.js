25df72cd55d6f71eb7ea217b49610827eb700472
Code Sandbox <hello@codesandbox.io>
2022-08-18 10:24:38 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b9983d8ed82d1d18ecaf009acafc7368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98c36fb1e0dac705449284670120bb1",
+    "url": "https://3ngin33r0d1y.github.io/csb-oeubof/index.html"
+  },
+  {
+    "revision": "4633a26c7df277941201",
+    "url": "https://3ngin33r0d1y.github.io/csb-oeubof/static/css/main.dae6170a.chunk.css"
+  },
+  {
+    "revision": "ebd047f1f59a72f16371",
+    "url": "https://3ngin33r0d1y.github.io/csb-oeubof/static/js/2.fa855660.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "https://3ngin33r0d1y.github.io/csb-oeubof/static/js/2.fa855660.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4633a26c7df277941201",
+    "url": "https://3ngin33r0d1y.github.io/csb-oeubof/static/js/main.8a1913ff.chunk.js"
+  },
+  {
+    "revision": "a8140cf56d0e28079337",
+    "url": "https://3ngin33r0d1y.github.io/csb-oeubof/static/js/runtime-main.09c0d578.js"
+  }
+]);
